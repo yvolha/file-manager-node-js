e511b4010df2ea34343933f84648e954fa176f17
Volha Yakauleva <yvvolha@gmail.com>
2024-10-10 21:38:18 +0300

    feat: add some helpers and const text

---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+export const OPERATION_FAILED_TEXT = 'Operation failed';
+
+export const INVALID_INPUT_TEXT = 'Invalid input';
+
+export const PROMPT_TEXT = 'Please put in a command.';
---
+++ src/utils/get-text.js
@@ -0,0 +1,11 @@
+export function getGreetingText(username){
+    return `Welcome to the File Manager, ${username}!`;
+}
+
+export function getGoodbyeText(username){
+    return `Thank you for using File Manager, ${username}, goodbye!`;
+}
+
+export function getCurrentDirText(directoryPath){
+    return `You are currently in ${directoryPath}`;
+}
