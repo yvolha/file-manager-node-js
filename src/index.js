dafdd92292f3775a8384b98bccf5cd78800eaa7e
Volha Yakauleva <yvvolha@gmail.com>
2024-10-11 23:59:27 +0300

    refactor: update name further

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { getCurrentDir } from "./components/get-current-dir.js";
-import username, { getUsername } from "./components/get-username.js";
+import username from "./components/get-username.js";
+import launchReadline from "./components/launch-readline.js";
 import { NODE_WARNING } from "./utils/constants.js";
 import { printBlueText, printMagentaText } from "./utils/get-color-coded-text.js";
 import { getGreetingText } from "./utils/get-text.js";
@@ -9,6 +10,8 @@ async function startApp(){
 
     printMagentaText(getGreetingText(username));
     console.log(await getCurrentDir());
+
+    launchReadline();
 
 } 
 
