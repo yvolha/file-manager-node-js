46c7928a58bdd277b351b846fd815714aa62dce3
Volha Yakauleva <yvvolha@gmail.com>
2024-10-12 19:22:44 +0300

    refactor: update file-directory sorting logic

--- src/commands/navigation/navigation-handlers.js
+++ src/commands/navigation/navigation-handlers.js
@@ -5,28 +5,17 @@ const TYPE_DIRECTORY = 'directory';
 const TYPE_FILE = 'file';
 
 export const cmdLs = async (directory) => {
-    console.log(directory);
   const items = await readdir(directory, {
     withFileTypes: true,
   })
-  .catch(() => console.error(OPERATION_FAILED_TEXT));
-
-  console.log(items);
+    .catch(() => console.error(OPERATION_FAILED_TEXT));
 
   const typedItems = await Promise.all(items
-  .map(item => ({
-    Name: item.name,
-    Type: item.isDirectory() ? TYPE_DIRECTORY : TYPE_FILE,
-  }))
-  .sort((a, b) => {
-    if (a.Type > b.Type) {
-      return 1;
-    }
-    if (a.Type < b.Type) {
-      return -1;
-    }
-    return 0;
-  }))
+    .map(item => ({
+        Name: item.name,
+        Type: item.isDirectory() ? TYPE_DIRECTORY : TYPE_FILE,
+    }))
+    .sort((a, b) => a.Type.localeCompare(b.Type)))
 
   console.table(typedItems);
 }
